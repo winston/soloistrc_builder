2e27b34ae724e56e9ce09814b4e865791bff7945
Winston <winston.yongwei@gmail.com>
2012-12-19 17:57:23 +0800

    Hide access token. Convert tabs to spaces.

--- public/application.js
+++ public/application.js
@@ -1,77 +1,77 @@
 $(function() {
 
-	function github_user() {
-		return $("#recipes").data("url").split(/[\/]+/)[2];
-	}
+  function github_user() {
+    return $("#recipes").data("url").split(/[\/]+/)[2];
+  }
 
-	function github_repo() {
-		return $("#recipes").data("url").split(/[\/]+/)[3];
-	}
+  function github_repo() {
+    return $("#recipes").data("url").split(/[\/]+/)[3];
+  }
 
-	function github_api_url() {
-		return "https://api.github.com/repos/" + github_user() + "/" + github_repo() + "/contents/recipes?access_token=51bfa46ab96268f721d05bcc9aae75090e84ef8e";
-	}
+  function github_api_url() {
+    return "https://api.github.com/repos/" + github_user() + "/" + github_repo() + "/contents/recipes";
+  }
 
-	$.ajax({
-	  url: github_api_url(),
-    dataType: 'jsonp',
-		success: function (response) {
-			$("#config").html("Recipes successfully retrieved from: " + github_api_url());
-			var $ul = $("#recipes ul");
-			$.each(response.data, function(index, elem) {
-					$ul.append(
-						"<li class='recipe'>" +
-							"<label><input type='checkbox' value='"+ elem.name +"'>" + elem.name + "</label>"	+
-							"<a class='source' href='"+ elem._links.self + "?access_token=51bfa46ab96268f721d05bcc9aae75090e84ef8e" + "'>View</a>" +
-						"</li>"
-					);
-			});
-		},
-		error: function(jqXHR, textStatus, errorThrown) {
-			$("#config").html("Hmm.. Failed to retrieved recipes from: " + github_api_url());
-		}
-	})
+  $.ajax({
+    headers: {"Authorization": "token 51bfa46ab96268f721d05bcc9aae75090e84ef8e"},
+    url: github_api_url(),
+    success: function (response) {
+      $("#config").html("Recipes successfully retrieved from: " + github_api_url());
 
-	$(".recipe label").live("click", function() {
-		var paths   = ["cookbook_paths:", "- cookbooks"]
-		var recipes = $.map($(".recipe input:checked"), function(elem, index) {
-			return "- " + github_repo() + "::" + $(elem).val().replace(".rb", "");
-		});
+      var $ul = $("#recipes ul");
+      $.each(response, function(index, elem) {
+          $ul.append(
+            "<li class='recipe'>" +
+              "<label><input type='checkbox' value='"+ elem.name +"'>" + elem.name + "</label>" +
+              "<a class='source' href='"+ elem._links.self + "'>View</a>" +
+            "</li>"
+          );
+      });
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+      $("#config").html("Hmm.. Failed to retrieved recipes from: " + github_api_url());
+    }
+  })
 
-		$("textarea").val(
-			paths.join("\n") +
-			"\n\n"					 +
-			"recipes:\n" 		 +
-			recipes.join("\n")
-		);
-	})
+  $(".recipe label").live("click", function() {
+    var paths   = ["cookbook_paths:", "- cookbooks"]
+    var recipes = $.map($(".recipe input:checked"), function(elem, index) {
+      return "- " + github_repo() + "::" + $(elem).val().replace(".rb", "");
+    });
 
-	$(".source").live("click", function(e) {
-		var url = $(this).attr("href");
-		$.ajax({
-		  headers: {"Authorization": "token 51bfa46ab96268f721d05bcc9aae75090e84ef8e"},
-		  url: url,
-		  dataType: 'jsonp',
-			success: function (response) {
-				$("#recipes,#soloistrc").fadeOut("fast")
+    $("textarea").val(
+      paths.join("\n") +
+      "\n\n"           +
+      "recipes:\n"     +
+      recipes.join("\n")
+    );
+  })
 
-				$("#source div").html(response.data.name);
-				$("#source pre").html(window.atob(response.data.content.replace(/\n/g, "")));
-				$("#source").show();
-			},
-			error: function(jqXHR, textStatus, errorThrown) {
-				$("#config").html("Hmm.. Failed to retrieve source from: " + url);
-			}
-		})
+  $(".source").live("click", function(e) {
+    var url = $(this).attr("href");
+    $.ajax({
+      headers: {"Authorization": "token 51bfa46ab96268f721d05bcc9aae75090e84ef8e"},
+      url: url,
+      success: function (response) {
+        $("#recipes,#soloistrc").fadeOut("fast")
 
-		e.preventDefault();
-	})
+        $("#source div").html(response.name);
+        $("#source pre").html(window.atob(response.content.replace(/\n/g, "")));
+        $("#source").show();
+      },
+      error: function(jqXHR, textStatus, errorThrown) {
+        $("#config").html("Hmm.. Failed to retrieve source from: " + url);
+      }
+    })
 
-	$("#source .close").live("click", function(e) {
-		$("#recipes,#soloistrc").fadeIn("fast");
-		$("#source").hide();
+    e.preventDefault();
+  })
 
-		e.preventDefault();
-	})
+  $("#source .close").live("click", function(e) {
+    $("#recipes,#soloistrc").fadeIn("fast");
+    $("#source").hide();
+
+    e.preventDefault();
+  })
 
 });
