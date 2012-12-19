2be1b11579289474a0092f53f0fab387246a6d9e
Winston <winston.yongwei@gmail.com>
2012-12-19 12:50:41 +0800

    Add access token to api calls to up rate limit from 60/hr to 5000/hr.

--- public/application.js
+++ public/application.js
@@ -9,21 +9,20 @@ $(function() {
 	}
 
 	function github_api_url() {
-		return "https://api.github.com/repos/" + github_user() + "/" + github_repo() + "/contents/recipes";
+		return "https://api.github.com/repos/" + github_user() + "/" + github_repo() + "/contents/recipes?access_token=51bfa46ab96268f721d05bcc9aae75090e84ef8e";
 	}
 
 	$.ajax({
 	  url: github_api_url(),
-	  dataType: 'jsonp',
+    dataType: 'jsonp',
 		success: function (response) {
 			$("#config").html("Recipes successfully retrieved from: " + github_api_url());
-
 			var $ul = $("#recipes ul");
 			$.each(response.data, function(index, elem) {
 					$ul.append(
 						"<li class='recipe'>" +
 							"<label><input type='checkbox' value='"+ elem.name +"'>" + elem.name + "</label>"	+
-							"<a class='source' href='"+ elem._links.self +"'>View</a>" +
+							"<a class='source' href='"+ elem._links.self + "?access_token=51bfa46ab96268f721d05bcc9aae75090e84ef8e" + "'>View</a>" +
 						"</li>"
 					);
 			});
@@ -50,6 +49,7 @@ $(function() {
 	$(".source").live("click", function(e) {
 		var url = $(this).attr("href");
 		$.ajax({
+		  headers: {"Authorization": "token 51bfa46ab96268f721d05bcc9aae75090e84ef8e"},
 		  url: url,
 		  dataType: 'jsonp',
 			success: function (response) {
